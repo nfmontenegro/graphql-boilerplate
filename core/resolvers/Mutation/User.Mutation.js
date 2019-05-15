3509345adc01a04656fe5a6b2ad924202bdacfcb
Nicolás <nicolas.fmontenegro@gmail.com>
2019-05-14 22:16:59 -0400

    docker compose config

--- core/resolvers/Mutation/User.Mutation.js
+++ core/resolvers/Mutation/User.Mutation.js
@@ -1,7 +1,9 @@
 export default {
   Mutation: {
-    async post(_, args, context, info) {
-      return context.prisma.createUser(args)
+    async post(_, args, {prisma}, info) {
+      return prisma.createUser({
+        ...args
+      })
     }
   }
 }
--- core/server.js
+++ core/server.js
@@ -1,11 +1,8 @@
-import dotenv from 'dotenv'
 import {GraphQLServer} from 'graphql-yoga'
 
 import {default as resolvers} from './resolvers'
 
 const {prisma} = require('./generated/prisma-client')
-
-dotenv.config()
 
 const server = new GraphQLServer({
   typeDefs: __dirname + '/schema.graphql',
